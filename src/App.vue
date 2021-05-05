1474fae1caae45a29fcd4da6ea74511f567daca8
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-05 10:36:52 -0300

    estilizando

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos" :key="foto">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto">
+        <div class="painel">
+          <h2 class="painel-titulo"> {{ foto.titulo }} </h2>
+          <div class="painel-conteudo">
+
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+
+          </div>
+        </div>
       </li>
     </ul>
     
@@ -16,23 +23,71 @@ export default {
   data(){
 
     return {
+
       titulo: "AluraPic",
       fotos: [
-        {
-        url: "https://media.gazetadopovo.com.br/viver-bem/2018/11/dog3-768x587-0e01be20.jpg",
-        titulo: "cachorro"
-        },
-        {
-        url: "https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg",
-        titulo: "cachorro 2"
-        },
+        
       ]
     }
+  },
+  created(){
+
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise
+      .then(res => res.json())
+      .then(fotosApi => this.fotos = fotosApi, err => console.log(err));
+
 
   }
+
 }
 </script>
 
 <style>
+
+.corpo{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado{
+  text-align: center;
+}
+
+.lista-fotos{
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item{
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+/* estilo do painel */ 
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 
 </style>
