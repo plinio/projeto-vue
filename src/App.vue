958ea4756a2702773c05942abceb71a5ae39b0b8
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-05 11:05:37 -0300

    escopo de estilo

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,11 @@
     <h1 class="centralizado">{{titulo}}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos" :key="foto">
-        <div class="painel">
-          <h2 class="painel-titulo"> {{ foto.titulo }} </h2>
-          <div class="painel-conteudo">
 
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
 
-          </div>
-        </div>
       </li>
     </ul>
     
@@ -19,7 +16,16 @@
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+
+    'meu-painel': Painel
+  },
+
   data(){
 
     return {
@@ -65,29 +71,6 @@ export default {
 
 .imagem-responsiva {
   width: 100%;
-}
-
-/* estilo do painel */ 
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
 }
 
 </style>
