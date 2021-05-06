85e3efc5b97534c2cdabcc4978f9d764594275c6
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-06 10:43:22 -0300

    componentizando a imagem responsiva

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto">
 
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
         </meu-painel>
 
       </li>
@@ -20,12 +20,14 @@
 <script>
 
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
 
   components: {
 
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data(){
@@ -82,10 +84,6 @@ export default {
 
 .lista-fotos .lista-fotos-item{
   display: inline-block;
-}
-
-.imagem-responsiva {
-  width: 100%;
 }
 
 .filtro{
