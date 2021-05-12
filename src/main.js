d041689acd5539bf3b1b898b3100c172121d14b1
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-12 08:52:49 -0300

    O módulo VeeValidate: npm install vee-validate@2.0.0-beta.18 --save

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import {routes} from './routes';
-import './directives/Transform'
+import './directives/Transform';
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000'
@@ -13,6 +14,8 @@ const router = new VueRouter({
   routes: routes,
   mode: 'history'
 })
+
+Vue.use(VeeValidate);
 
 new Vue({
   el: '#app',
