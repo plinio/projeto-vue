ae125c6b8e32b1b45ae3c1767a2d25ecea15632a
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-12 09:36:23 -0300

    Code splitting e lazy loading e npm run build para gerer o build particionado

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Home from './components/home/Home.vue';
-import Cadastro from './components/cadastro/Cadastro.vue';
+//import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
 
 export const routes = [
 
