deeba16e402909105cf604191e2e366ce8ae91fd
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-13 10:16:23 -0300

    npm install css-loader@0.25.0 style-loader@0.13.1 --save-dev
    importando css do bootstrap
    
    npm install url-loader@0.5.7 --save-dev

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import './directives/Transform';
 import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000'
 Vue.use(VueRouter);
