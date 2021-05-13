41168c68633cee88375a8510de38b2246c37f831
Plinio Garcia <plinioagarcia@gmail.com>
2021-05-13 10:20:03 -0300

    Importando seus arquivos CSS

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000'
